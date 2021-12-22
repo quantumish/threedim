9c3721022a5a923bdeec1f74fc875a79e0c87a73
David Freifeld <freifeld.david@gmail.com>
2021-12-21 20:39:31 -0800

    Sketchy developments with collisions

--- src/engine.rs
+++ src/engine.rs
@@ -3,78 +3,78 @@ pub use kiss3d::nalgebra::Vector3;
 
 #[derive(Clone)]
 pub struct LinearState {
-	pub position: Vector3<f64>,
-	pub momentum: Vector3<f64>,
-	pub velocity: Vector3<f64>,
-	pub mass: f64,
-	pub inv_mass: f64
+    pub position: Vector3<f64>,
+    pub momentum: Vector3<f64>,
+    pub velocity: Vector3<f64>,
+    pub mass: f64,
+    pub inv_mass: f64
 }
 
 impl LinearState {
-	fn recalculate(&mut self) {
-		self.velocity = self.momentum * self.inv_mass;
-	}
-	
-	fn force(&self, t: f64) -> Vector3<f64> {
-		return Vector3::new(0.0, -9.8, 0.0);
-	}
+    fn recalculate(&mut self) {
+	self.velocity = self.momentum * self.inv_mass;
+    }
+    
+    fn force(&self, t: f64) -> Vector3<f64> {
+	return Vector3::new(0.0, -9.8, 0.0);
+    }
 
 }
 
 pub struct LinearDerivative {
-	pub velocity: Vector3<f64>,
-	pub force: Vector3<f64>,
+    pub velocity: Vector3<f64>,
+    pub force: Vector3<f64>,
 }
 
 struct RotState {
-	
+    
 }
 
 struct RotDerivative {
-	
+    
 }
 
 pub fn eval(initial: &LinearState, t: f64, dt: f64, d: &LinearDerivative) -> LinearDerivative {
-	let mut state: LinearState = initial.clone();
-	state.position = initial.position + d.velocity * dt;
-	state.velocity = initial.momentum / initial.mass;
-	state.momentum = initial.momentum + d.force * dt;
+    let mut state: LinearState = initial.clone();
+    state.position = initial.position + d.velocity * dt;
+    state.velocity = initial.momentum / initial.mass;
+    state.momentum = initial.momentum + d.force * dt;
 
-	let out: LinearDerivative = LinearDerivative {
-		velocity: state.velocity,
-		force: initial.force(t+dt),
-	};
-	return out;
+    let out: LinearDerivative = LinearDerivative {
+	velocity: state.velocity,
+	force: initial.force(t+dt),
+    };
+    return out;
 }
 
 pub fn integrate(state: &mut LinearState, t: f64, dt: f64) -> Vector3<f64> {
-	let (a, b, c, d): (LinearDerivative, LinearDerivative, LinearDerivative, LinearDerivative);
-	a = eval(state, t, 0.0, &LinearDerivative {velocity: Vector3::new(0.0,0.0,0.0),
-											   force: Vector3::new(0.0,0.0,0.0)});
-	b = eval(state, t, dt*0.5, &a);
-	c = eval(state, t, dt*0.5, &b);
-	d = eval(state, t, dt, &c);
+    let (a, b, c, d): (LinearDerivative, LinearDerivative, LinearDerivative, LinearDerivative);
+    a = eval(state, t, 0.0, &LinearDerivative {velocity: Vector3::new(0.0,0.0,0.0),
+					       force: Vector3::new(0.0,0.0,0.0)});
+    b = eval(state, t, dt*0.5, &a);
+    c = eval(state, t, dt*0.5, &b);
+    d = eval(state, t, dt, &c);
 
-	let dxdt = 1.0 / 6.0 * (a.velocity + 2.0 * (b.velocity + c.velocity) + d.velocity);
-	let dvdt = 1.0 / 6.0 * (state.momentum/state.mass + 2.0 * (state.momentum/state.mass + state.momentum/state.mass) + state.momentum/state.mass);
-	let dmdt = 1.0 / 6.0* (a.force + 2.0 * (b.force + c.force) + d.force);
+    let dxdt = 1.0 / 6.0 * (a.velocity + 2.0 * (b.velocity + c.velocity) + d.velocity);
+    let dvdt = 1.0 / 6.0 * (state.momentum/state.mass + 2.0 * (state.momentum/state.mass + state.momentum/state.mass) + state.momentum/state.mass);
+    let dmdt = 1.0 / 6.0* (a.force + 2.0 * (b.force + c.force) + d.force);
 
-	state.position += dxdt * dt;
-	state.velocity += dvdt * dt;
-	state.momentum += dmdt * dt;
-	return dxdt * dt;
+    state.position += dxdt * dt;
+    state.velocity += dvdt * dt;
+    state.momentum += dmdt * dt;
+    return dxdt * dt;
 }
 
 #[derive(Clone, Copy)]
 pub enum Shape {
-	Cube(Vector3<f64>),
-	// Sphere(f32),
+    Cube(Vector3<f64>),
+    // Sphere(f32),
 }
 
 #[derive(Clone)]
 pub struct Object {
-	pub node: SceneNode,
-	pub lstate: LinearState,
-	pub shape: Shape,
-	pub immovable: bool,
+    pub node: SceneNode,
+    pub lstate: LinearState,
+    pub shape: Shape,
+    pub immovable: bool,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 pub mod engine;
 pub use crate::engine::*;
-extern crate kiss3d;
-extern crate alloc;
+
 use kiss3d::light::Light;
-use kiss3d::scene::SceneNode;
 use kiss3d::window::{State, Window};
 use kiss3d::nalgebra::{UnitQuaternion, Vector3, Point2, Point3, Translation3};
 use kiss3d::text::Font;
@@ -13,90 +11,113 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-	#[wasm_bindgen(js_namespace = console)]
-	fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 const dt: f64 = 0.001;
 
 struct AppState {
     objs: Vec<Object>,
-	t: f64,	
+    t: f64,	
 }
-
-fn colliding(a: &Object, b: &Object) -> bool {
-	match (a.shape, b.shape) {
-		(Shape::Cube(v1), Shape::Cube(v2)) => {
-			let a_min = a.lstate.position-(v1 * 0.5);
-			let a_max = a.lstate.position+(v1 * 0.5);
-			let b_min = b.lstate.position-(v2 * 0.5);
-			let b_max = b.lstate.position+(v2 * 0.5);
-			log(format!("{} {} {} {}", a_min, b_max, a_max, b_min).as_str());
-			log(format!("{} {}", a_min >= b_max, a_max <= b_min).as_str());			
-			a_min >= b_max && a_max <= b_min
-		},
-	}
+ 
+fn colliding(a: &Object, b: &Object) -> Option<Vector3<f32>> {
+    match (a.shape, b.shape) {
+	(Shape::Cube(v1), Shape::Cube(v2)) => {
+	    let mut overlaps: Vec<f32> = Vec::new();
+	    for i in 0..3 {
+		let overlap = (a.lstate.position[i] - b.lstate.position[i]).abs();
+		if overlap > (v1[i] + v2[i]) {
+		    return None;
+		}
+		overlaps.push(overlap as f32);		
+	    }
+	    let mut min = 0;
+	    for i in 0..overlaps.len() {
+		if overlaps[i] < overlaps[min] {
+		    min = i;
+		}
+	    }
+	    let mut out = Vector3::zeros();
+	    out[min] = overlaps[min];
+	    return Some(out);
+	},
+    }
 }
 
 impl State for AppState {
     fn step(&mut self, w: &mut Window) {
-		for obj in self.objs.iter_mut() {
-			if obj.immovable {continue;}
-			integrate(&mut obj.lstate, self.t, dt);
-			obj.node.set_local_translation(Translation3::new(obj.lstate.position.x as f32, obj.lstate.position.y as f32, obj.lstate.position.z as f32));
+	for obj in self.objs.iter_mut() {
+	    if obj.immovable {continue;}
+	    integrate(&mut obj.lstate, self.t, dt);
+	    obj.node.set_local_translation(
+		Translation3::new(
+		    obj.lstate.position.x as f32,
+		    obj.lstate.position.y as f32,
+		    obj.lstate.position.z as f32
+		)
+	    );
+	}
+	let tmp = self.objs.clone();
+	let l = self.objs.len();
+	for i in 0..l {
+	    for j in 0..l {
+		if j == i { continue; }
+		if let Some(overlap) = colliding(&self.objs[i], &tmp[j]) {
+		    if self.objs[i].immovable {
+			continue;
+		    } else {
+			self.objs[i].node.append_translation(&Translation3::from(overlap));
+			self.objs[i].node.set_color(0.0, 1.0, 0.0);
+		    }
 		}
-		let mut tmp = self.objs.clone();
-		let l = self.objs.len();
-		for i in 0..l {
-			if colliding(&self.objs[i], &tmp[i]) {
-				log(format!("HELLO {}", self.t).as_str());
-			}
-		}
-		log("FOREVA");
+	    }
+	}
     }
 }
 
 #[wasm_bindgen(start)]
 pub fn main() {	
     let mut window = Window::new("Kiss3d: wasm example");
-	let mut objs: Vec<Object> = Vec::new();
-	
+    let mut objs: Vec<Object> = Vec::new();
+    
     objs.push(Object {
-		node: window.add_cube(0.1, 0.1, 0.1),
-		lstate: LinearState {
-			position: Vector3::new(0.0, 0.0, 0.0),
-			velocity: Vector3::new(0.0, 0.0, 0.0),
-			momentum: Vector3::new(0.0, -100.0, 0.0),
-			mass: 100.0,
-			inv_mass: 1.0, 
-		},
-		shape: Shape::Cube(Vector3::new(0.1, 0.1, 0.1)),
-		immovable: false,
-	});
-	let tmp = objs.len();
+	node: window.add_cube(0.1, 0.1, 0.1),
+	lstate: LinearState {
+	    position: Vector3::new(0.0, 0.0, 0.0),
+	    velocity: Vector3::new(0.0, 0.0, 0.0),
+	    momentum: Vector3::new(0.0, -100.0, 0.0),
+	    mass: 100.0,
+	    inv_mass: 1.0, 
+	},
+	shape: Shape::Cube(Vector3::new(0.1, 0.1, 0.1)),
+	immovable: false,
+    });
+    let tmp = objs.len();
     objs[tmp-1].node.set_color(97.0/255.0, 97.0/255.0, 97.0/255.0);
 
-	objs.push(Object {
-		node: window.add_cube(0.5, 0.02, 0.5),
-		lstate: LinearState {
-			position: Vector3::new(0.0, 0.0, 0.0),
-			velocity: Vector3::new(0.0, 0.0, 0.0),
-			momentum: Vector3::new(0.0, 0.0, 0.0),
-			mass: 10.0,
-			inv_mass: 0.0, 
-		},
-		shape: Shape::Cube(Vector3::new(0.5, 0.02, 0.5)),
-		immovable: true,
-	});
-	let tmp = objs.len();
-	objs[tmp-1].node.set_local_translation(Translation3::new(0.0, -0.5, 0.0));
+    objs.push(Object {
+	node: window.add_cube(0.5, 0.02, 0.5),
+	lstate: LinearState {
+	    position: Vector3::new(0.0, 0.0, 0.0),
+	    velocity: Vector3::new(0.0, 0.0, 0.0),
+	    momentum: Vector3::new(0.0, 0.0, 0.0),
+	    mass: 10.0,
+	    inv_mass: 0.0, 
+	},
+	shape: Shape::Cube(Vector3::new(0.5, 0.02, 0.5)),
+	immovable: true,
+    });
+    let tmp = objs.len();
+    objs[tmp-1].node.set_local_translation(Translation3::new(0.0, -0.5, 0.0));
 
-	
+    
     window.set_light(Light::StickToCamera);
-	window.set_background_color(17.0/255.0, 19.0/255.0, 19.0/255.0);
+    window.set_background_color(17.0/255.0, 19.0/255.0, 19.0/255.0);
 
     let v: Translation3<f32> = Translation3::new(0.0, -0.001, 0.0);
     let state = AppState { objs: objs, t: 0.0 };
-	
+    
     window.render_loop(state)
 }
